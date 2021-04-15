66211a4bcc158fcbb1e63204e975ac3f3a7fcf6c
BayLadyCoder <rachada_32@hotmail.com>
2021-04-14 22:23:54 -0400

    Created GlobalStyle and NavBarContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Nav from "./components/NavBar/";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <h1>Hello World!</h1>
     </div>
   );
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Nav, NavBarContainer } from "./NavBar.styled";
+
+const NavBar = () => {
+  return (
+    <Nav>
+      <NavBarContainer>Test</NavBarContainer>
+    </Nav>
+  );
+};
+
+export default NavBar;
