8ba2cf1f2a957fc6d1311339d5ac586c2dcae6b9
BayLadyCoder <rachada_32@hotmail.com>
2021-04-14 22:45:21 -0400

    Add MobileIcon on small device

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,16 +1,35 @@
-import React from "react";
-import { Nav, NavBarContainer, NavLogo, NavIcon } from "./NavBar.styled";
+import React, { useState } from "react";
+import {
+  Nav,
+  NavBarContainer,
+  NavLogo,
+  NavIcon,
+  MobileIcon,
+} from "./NavBar.styled";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
 
 const NavBar: React.FC = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
   return (
-    <Nav>
-      <NavBarContainer>
-        <NavLogo to="/">
-          <NavIcon />
-          ULTRA
-        </NavLogo>
-      </NavBarContainer>
-    </Nav>
+    <>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <Nav>
+          <NavBarContainer>
+            <NavLogo to="/">
+              <NavIcon />
+              ULTRA
+            </NavLogo>
+            <MobileIcon onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </MobileIcon>
+          </NavBarContainer>
+        </Nav>
+      </IconContext.Provider>
+    </>
   );
 };
 
