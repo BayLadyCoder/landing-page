6b5b2af83108029cc198805ce1fbdbfe069fdaaa
BayLadyCoder <rachada_32@hotmail.com>
2021-04-17 16:20:18 -0400

    Added SVGs and other three sections

--- src/pages/Data.ts
+++ src/pages/Data.ts
@@ -14,3 +14,54 @@ export const homeObjOne = {
   alt: "Image",
   start: "",
 };
+
+export const homeObjTwo = {
+  primary: false,
+  lightBg: true,
+  imgStart: "start",
+  topLine: "marketing Agency",
+  headline: "Lead Generation Specialist for Online Business",
+  description:
+    "We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.",
+  buttonLabel: "Get Started",
+  lightTopLine: false,
+  lightText: false,
+  lightTextDesc: false,
+  img:
+    "https://images0.westend61.de/0000727540pw/portrait-of-confident-businesswoman-in-office-CHAF001779.jpg",
+  alt: "Image",
+  start: "true",
+};
+
+export const homeObjThree = {
+  primary: true,
+  lightBg: false,
+  imgStart: "",
+  topLine: "marketing Agency",
+  headline: "Lead Generation Specialist for Online Business",
+  description:
+    "We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.",
+  buttonLabel: "Get Started",
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  img: require("../images/hiring.svg").default,
+  alt: "Image",
+  start: "",
+};
+export const homeObjFour = {
+  primary: false,
+  lightBg: true,
+  imgStart: "start",
+  topLine: "marketing Agency",
+  headline: "Lead Generation Specialist for Online Business",
+  description:
+    "We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.",
+  buttonLabel: "Get Started",
+  lightTopLine: false,
+  lightText: false,
+  lightTextDesc: false,
+  img: require("../images/investing.svg").default,
+  alt: "Image",
+  start: "true",
+};
